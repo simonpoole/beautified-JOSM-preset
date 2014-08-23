5e41c7e70f99120ed71127202cce57130c8bf466
simon <simon@server2.poole.ch>
2014-08-23 11:35:18 +0200

    More reorg

--- icons/tools/generatepng-osm-icons.sh
+++ icons/tools/generatepng-osm-icons.sh
@@ -6,7 +6,7 @@ BASEFOLDER=`pwd`;
 popd  > /dev/null
 BASEFOLDER=`dirname $BASEFOLDER`
 
-TYPES=(             'accommodation' 'amenity' 'barrier' 'education' 'food'    'health'  'landuse' 'money'   'natural'	'place_of_worship' 'poi'	'power'    'shopping' 'sport'   'tourist' 'transport' 'water'	'way')
+TYPES=(             'accommodation' 'amenity' 'barrier' 'education' 'food'    'health'  'landuse' 'money'   'natural'	'place_of_worship' 'poi'	'power'    'shopping' 'sport'   'tourist' 'transport' 'water'	'highway')
 FORGROUND_COLOURS=( '#0092DA'       '#734A08' '#666666' '#39AC39'   '#734A08' '#DA0092' '#999999' '#000000' '#999999'	'#000000'          '#000000'	'#8e7409'  '#AC39AC'  '#39AC39' '#734A08' '#0092DA'   '#0092DA' '#666666')
 
 SIZES=( 32 )
