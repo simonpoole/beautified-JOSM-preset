60b4bb2c6e79ee5e5004bc8e06f37a525f8d2929
Simon Poole <simon@poole.ch>
2015-11-03 11:41:20 +0100

    Further improvement social_facility preset, added transifex configuration to pull translations and added them to the zip

--- scripts/vespucci_zip.sh
+++ scripts/vespucci_zip.sh
@@ -7,3 +7,6 @@ rm  vespucci_zip.zip
 zip vespucci_zip.zip vespucci_zip_preset.xml
 cd ../icons/png
 ls *.png | zip -@ ../../gen/vespucci_zip.zip 
+cd ../../i8n
+ls *.po | zip -@ ../gen/vespucci_zip.zip 
+
