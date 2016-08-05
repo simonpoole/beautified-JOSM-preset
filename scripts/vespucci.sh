6333296c5415b27cddc02c84e7c6a574146faf56
Simon Poole <simon@poole.ch>
2016-08-05 16:33:09 +0200

    Generate icons zip archive.

--- scripts/vespucci.sh
+++ scripts/vespucci.sh
@@ -2,5 +2,6 @@
 
 
 sed "s=ICONPATH:=icons/png/=" < master_preset.xml | sed "s/ICONTYPE/png/" > gen/vespucci_preset.xml
-cd ../icons/png
+cd icons/png
+rm ../../gen/vespucci_icons.zip
 ls *.png | zip -@ ../../gen/vespucci_icons.zip 
