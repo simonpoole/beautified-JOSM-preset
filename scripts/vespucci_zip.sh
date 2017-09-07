5fa25fbabf5cb731fa3a94f3c40452bf9d579e3f
simonpoole <simon@poole.ch>
2017-09-07 11:46:48 +0200

    Create a zip file without translations

--- scripts/vespucci_zip.sh
+++ scripts/vespucci_zip.sh
@@ -7,6 +7,7 @@ rm  vespucci_zip.zip
 zip vespucci_zip.zip vespucci_zip_preset.xml
 cd ../icons/png
 ls *.png | zip -@ ../../gen/vespucci_zip.zip 
+cp ../../gen/vespucci_zip.zip ../../gen/vespucci_zip_no_translations.zip
 cd ../../i18n
 ls *.po | zip -@ ../gen/vespucci_zip.zip 
 
