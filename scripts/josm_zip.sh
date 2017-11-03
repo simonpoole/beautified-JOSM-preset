fea3f39e11178636b7ed9ec744cc5ced3e87b910
Simon Poole <simon@poole.ch>
2017-11-03 14:51:37 +0100

    Add validation for JOSM variant

--- scripts/josm_zip.sh
+++ scripts/josm_zip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed "s=ICONPATH:==" < master_preset.xml | sed "s/ICONTYPE/png/" | xmlstarlet tr toJOSM.xslt > gen/josm_preset.xml
+xmlstarlet val -s josm-preset-1.0.xlmns -e gen/josm_preset.xml || exit
 cd gen
 rm  josm.zip
 zip josm.zip josm_preset.xml
