afdcc120e059b0e977ccbba2ed03e5213068b11f
simonpoole <simon@poole.ch>
2017-11-14 21:08:52 +0100

    Validate full version of preset

--- scripts/vespucci.sh
+++ scripts/vespucci.sh
@@ -2,7 +2,7 @@
 
 
 sed "s=ICONPATH:=icons/png/=" < master_preset.xml | sed "s/ICONTYPE/png/" > gen/vespucci_preset.xml
-#xmlstarlet val -s josm-preset-1.0.xlmns -e gen/vespucci_preset.xml || exit
+xmlstarlet val -s vespucci-preset-1.0.xlmns -e gen/vespucci_preset.xml || exit
 cd icons/png
 rm ../../gen/vespucci_icons.zip
 ls *.png | zip -@ ../../gen/vespucci_icons.zip 
